afc5e31ffa75354a1e4c16d4340823f22a504cd8
SvarunBGS <bernatgene@gmail.com>
2018-04-04 09:18:34 +0200

    Fent main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,22 @@
 package main;
 
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        AreaRescat area = new AreaRescat(  );
+
+    }
+
+    private static void AreaRescatHillClimbing(AreaRescat area) throws Exception {
+        Problem problem = new Problem( area, new RescatSuccesorFunction(), new RescatGoalTest(),
+                new RescatHeuristicFunction() );
+        Search search = new HillClimbingSearch();
+        SearchAgent agent = new SearchAgent( problem, search);
     }
 }
+
