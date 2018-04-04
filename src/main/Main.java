666fef7778ceec03f5c95fda581232e14a1750f4
SvarunBGS <bernatgene@gmail.com>
2018-04-04 09:27:39 +0200

    Main fet2

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,21 @@ import aima.search.framework.Search;
 import aima.search.framework.SearchAgent;
 import aima.search.informed.HillClimbingSearch;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
 public class Main {
 
-    public static void main(String[] args) {
-        AreaRescat area = new AreaRescat(  );
+    public static void main(String[] args) throws Exception {
+        int numCentres, numGrups, seed, helisPerCentre;
+        numCentres = 5;
+        numGrups = 100;
+        seed = 2;
+        helisPerCentre = 1;
+        AreaRescat area = new AreaRescat(numGrups, numCentres, helisPerCentre, seed);
+
+        AreaRescatHillClimbing( area );
 
     }
 
@@ -17,6 +28,27 @@ public class Main {
                 new RescatHeuristicFunction() );
         Search search = new HillClimbingSearch();
         SearchAgent agent = new SearchAgent( problem, search);
+
+        System.out.println();
+        
+        printActions(agent.getActions());
+        printInstrumentation(agent.getInstrumentation());
+    }
+
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
+
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
     }
 }
 
